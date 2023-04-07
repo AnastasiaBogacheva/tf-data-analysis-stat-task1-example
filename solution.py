5e7b86e57ea85541d992dce430e0cbb0a7a07457
AnastasiaBogacheva <130046692+AnastasiaBogacheva@users.noreply.github.com>
2023-04-07 23:19:39 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
-
+from scipy.stats import gamma
 
 chat_id = 694882183 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array) -> float:
-    s = 0
-    for i in range(len(x)):
-      s = s + np.log(x[i]-247)
-    s = s/len(x)
-    return s # Ваш ответ
+def solution(p: float, x: np.array) -> tuple:
+    alpha = 1 - p
+    n = len(x)
+    c = 2/(n*44*44)
+    z1 = gamma.ppf(alpha/2,n)
+    z2 = gamma.ppf(1-alpha/2,n)
+    return (c*z1,c*z2)
